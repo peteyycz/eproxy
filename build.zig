b313f9d5d596fc385fcb36c2f97113fac4c0e4a6
Peter Czibik <peter.czibik@byborgenterprises.com>
2025-05-13 15:51:53 +0200

    Support macos vol1

--- build.zig
+++ build.zig
@@ -10,20 +10,36 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // Add the C file
-    exe.addCSourceFile(.{
-        .file = b.path("main.c"),
-        .flags = &[_][]const u8{
-            "-std=c11",
-            "-D_GNU_SOURCE",
-        },
-    });
+    // Common C flags
+    var c_flags = std.ArrayList([]const u8).init(b.allocator);
+    defer c_flags.deinit();
+
+    c_flags.append("-std=c11") catch unreachable;
 
     // Link with C standard library
     exe.linkLibC();
 
-    // Link with liburing
-    exe.linkSystemLibrary("uring");
+    // Conditionally link libraries based on target OS
+    switch (target.result.os.tag) {
+        .linux => {
+            c_flags.append("-D_GNU_SOURCE") catch unreachable;
+            c_flags.append("-DUSE_IO_URING=1") catch unreachable;
+            // On Linux, link with liburing
+            exe.linkSystemLibrary("uring");
+        },
+        .macos => {
+            // On macOS no need to explicitly link kqueue
+            // as it's part of the system libraries
+            c_flags.append("-DUSE_KQUEUE=1") catch unreachable;
+        },
+        else => {},
+    }
+
+    // Add the C file
+    exe.addCSourceFile(.{
+        .file = b.path("main.c"),
+        .flags = c_flags.items,
+    });
 
     b.installArtifact(exe);
 
